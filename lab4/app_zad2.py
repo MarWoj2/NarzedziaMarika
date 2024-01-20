55dfac57802ac214dd9bc35a48b851ef1dc3e336
Marika Wojtasik <marwoj022@pbs.edu.pl>
2024-01-20 01:22:23 +0100

    Poprawki zgodnie z wynikami analizy flake8

--- lab4/app_zad2.py
+++ lab4/app_zad2.py
@@ -1,17 +1,29 @@
 def add_numbers(a, b):
- return a+b
-def subtract_numbers(a , b):
- return a - b
-def multiply_numbers(a,b):
- return a* b
+    """Dodawanie dwóch liczb."""
+    return a + b
+
+def subtract_numbers(a, b):
+    """Odejmowanie drugiej liczby od pierwszej."""
+    return a - b
+
+def multiply_numbers(a, b):
+    """Mnożenie dwóch liczb."""
+    return a * b
+
 def divide_numbers(a, b):
- if b== 0:
- raise ValueError("Dzielenie przez zero jest niedozwolone.")
- return a /b
+    """Dzielenie pierwszej liczby przez drugą."""
+    if b == 0:
+        raise ValueError("Dzielenie przez zero jest niedozwolone.")
+    return a / b
+
+def main():
+    num1 = 10
+    num2 = 5
+    print(f"Dodawanie: {add_numbers(num1, num2)}")
+    print(f"Odejmowanie: {subtract_numbers(num1, num2)}")
+    print(f"Mnożenie: {multiply_numbers(num1, num2)}")
+    print(f"Dzielenie: {divide_numbers(num1, num2)}")
+
 if __name__ == "__main__":
- num1 = 10
- num2 = 5
- print(f"Dodawanie: {add_numbers(num1, num2)}")
- print(f"Odejmowanie: {subtract_numbers(num1,num2)}")
- print(f"Mnożenie: {multiply_numbers( num1, num2)}")
- print(f"Dzielenie: {divide_numbers(num1 , num2)}")
+    main()
+
